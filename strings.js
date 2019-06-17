d6ab70b85fd3bde988ee08982f9d920421462c60
Max Kelly <max@revelthat.com>
2019-06-17 11:53:13 +1000

    First node app deploy

---
+++ strings.js
@@ -0,0 +1,32 @@
+
+//A varible is a way to store a value for later.
+//the below is an example of a varible.
+
+//The below is a varible to get firstName
+let firstName = 'Max';
+//The below is a varible to get lastName
+let lastName = 'Kelly';
+//The below is a varible which combine the firstName and lastName together
+let fullName =  firstName + ' ' + lastName;
+
+//Below are examples of varibles I tested
+let city = 'Melbourne';
+let country = 'Australia';
+let location = city + ' ' + country;
+
+//----------------------------------------------//
+let work = 'TLA';
+let age = 21;
+
+
+
+
+//The below prints the above varible into the console.
+console.log(fullName);
+//or you can print fullname like the below.
+console.log(firstName + ' '  + lastName + ' ' + age);
+console.log(location);
+
+//If statements
+
+console.log(new Date().getHours() < 12 ? "Good Morning!" : "Time for a siesta");
